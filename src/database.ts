6872335510af70b967fff4a18e690af239762102
Jhonata Costa <jhomcosta@gmail.com>
2023-03-13 23:40:16 -0300

    aula zod

--- src/database.ts
+++ src/database.ts
@@ -1,15 +1,16 @@
+import { env } from './env'
 import { knex as setupKnex, Knex } from 'knex'
 
 export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: './db/app.db'
+    filename: env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
-    directory: './db/migrations'
-  }
+    directory: './db/migrations',
+  },
 }
 
 export const knex = setupKnex(config)
--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,26 @@
 import fastify from 'fastify'
 import crypto from 'node:crypto'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
 app.get('/hello', async (req, res) => {
-  const transaction = await knex('transactions').insert({
-    id: crypto.randomUUID(),
-    title: 'Teste',
-    amount: 1000
-  }).returning('*')
+  const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Teste',
+      amount: 1000,
+    })
+    .returning('*')
 
   return transaction
 })
 
-app.listen({
-  port: 3333
-}).then(() => {
-  console.log('listening on port 3333')
-})
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('listening on port 3333')
+  })
