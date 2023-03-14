54d98296138a8a7646e33f52044acddbed55c744
Jhonata Costa <jhomcosta@gmail.com>
2023-03-14 09:37:34 -0300

    aula plugins

---
+++ README.md
@@ -0,0 +1,12 @@
+# RF
+
+- [ ] O usuário deve poder criar uma nova transação;
+- [ ] O usuário deve poder obter um resumo da sua conta;
+- [ ] O usuário deve poder listar todas transações que ja ocorreram;
+- [ ] O usuário deve poder visualizar uma transação única;
+
+# RN
+
+- [ ] A transação pode ser do tipo crédito que somará ao valor total, ou débito que ira subtrair do total;
+- [ ] Deve ser possível identificar o usuário entre as requisições;
+- [ ] O usuário só pode visualizar transações o qual ele criou;
--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,11 @@
 import fastify from 'fastify'
-import crypto from 'node:crypto'
-import { knex } from './database'
 import { env } from './env'
+import { transactionRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async (req, res) => {
-  const transaction = await knex('transactions')
-    .insert({
-      id: crypto.randomUUID(),
-      title: 'Teste',
-      amount: 1000,
-    })
-    .returning('*')
-
-  return transaction
+app.register(transactionRoutes, {
+  prefix: '/transactions',
 })
 
 app
